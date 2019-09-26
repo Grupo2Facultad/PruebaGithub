8278c395420b0eb30c51d65c0201c488308f1a83
juanc <juanc@192.168.0.16>
2019-09-26 17:04:20 -0300

    ejemplo

--- GithubTest/src/githubtest/ActaParcial.java
+++ GithubTest/src/githubtest/ActaParcial.java
@@ -16,24 +16,15 @@ public class ActaParcial extends Acta{
     
     
 
-//    public List<Alumno> getHabilitados(Carrera carrera, PlanDeEstudio planDeEstudio, Asignatura asignatura,PeriodoLectivoConAño periodo,int numeroDeParcial) {
-//        ArrayList<PlanDeEstudio> planes=(ArrayList)carrera.getPlanDeEstudio();
-//        for (PlanDeEstudio plane : planes) {
-//            if (plane.equals(planDeEstudio)){
-//                ArrayList<Asignatura> asignaturas= plane.getAsignaturas();
-//                for (Asignatura asignatura1 : asignaturas) {
-//                    if (asignatura1.equals(asignatura)){
-//                        
-//                    }
-//                }
-//            }
-//        }
-//    }
-    //No Listo
-    public boolean verificarParcialAnterior () {
-        return false;
+public List<Alumno> getHabilitados(){
+    ArrayList<Alumno> habilitados=new ArrayList<Alumno>();  
+    for (InscripcionAExamen inscripcion : super.getInscripciones()) {
+        if (inscripcion.isHabilitado()){
+            habilitados.add(inscripcion.getAlumno());
+        }
     }
-
+    return habilitados;
+}
     @Override
     public void imprimirActa() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
