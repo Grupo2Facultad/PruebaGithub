8a190ac6a4896171a2a8a231a95dcaa3ccb5396b
juanc <juanc@192.168.0.16>
2019-10-03 13:42:04 -0300

    ejemplo

--- GithubTest/src/githubtest/Alumno.java
+++ GithubTest/src/githubtest/Alumno.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Alumno extends Persona{
-private String algo;
 private String domicilio,
         localidad,
         provincia,
